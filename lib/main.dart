479de80749468906219c3b4d83321c5ce0275689
CoderIshanGupta <guptaishan2609@gmail.com>
2025-10-12 01:27:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-
-
 import 'package:flutter/material.dart';
-
-
 import 'package:provider/provider.dart';
 import 'package:toastification/toastification.dart';
 
 import 'router.dart';
 import 'providers.dart';
-
+import '../providers/theme_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,14 +21,25 @@ class MyApp extends StatelessWidget {
         maxToastLimit: 1,
       ),
       child: MultiProvider(
-        providers: providers,
-        child: MaterialApp.router(
-          title: 'Nourish App',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          ),
-          routerConfig: router,
-          builder: (context, child) => child!,
+        providers: [
+          ...providers,
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ],
+        child: Consumer<ThemeProvider>(
+          builder: (context, themeProvider, child) {
+            return MaterialApp.router(
+              title: 'Nourish App',
+              themeMode: themeProvider.themeMode, // Use the persisted themeMode
+              theme: ThemeData.light().copyWith( // Define light theme
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+              darkTheme: ThemeData.dark().copyWith( // Define dark theme
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+              routerConfig: router,
+              builder: (context, child) => child!,
+            );
+          },
         ),
       ),
     );
